e8daf2410ac9d5038ecbe2a2efb36485bdcca3cf
bhunos <bhuno.souzaa@gmail.com>
2021-12-16 14:51:20 -0300

    banco de dados faunadb

--- src/components/SinginButton/index.tsx
+++ src/components/SinginButton/index.tsx
@@ -1,25 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from "./styles.module.scss"
 
 export function SigninButton() {
-  const isUserLoggerIn = true;
+  const { data: session } = useSession()
 
+  console.log(session)
 
-  return isUserLoggerIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signinButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Bhunos
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signinButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sing in with Github
